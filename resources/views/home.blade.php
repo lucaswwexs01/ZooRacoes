53f1bb23c66f79d97fd8bdd06e1ca23a4ac64e78
LUCAS QUEIROZ <lucaswwexs@gmail.com>
2024-11-19 09:25:20 -0300

    implementação bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,70 @@
 @extends('layout.menu')
 @section('content')
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-<div> <!--Baner inicial -->
 
-        <div>
-            <div class="responsive">
+    <div class="container-fluid" style="overflow: hidden"> <!--Container-->
+        <div class="row" style="overflow: hidden"><!--Baner inicial -->
+            <div class="col"> <!--Coluna 1-->
                 <h3 class="h3-banner">ZooRações</h3>
-                <h1 class="texto-gradiente">Cuide do Bem-Estar do seu</h1>
-                <h1 class="texto-gradiente texto-correcao">Amigo de 4 patas !</h1>
-                <p class="paragrafo-gradiente ">Encontre a melhor seleção de rações e medicamentos para <br> manter seu animal saudável e feliz.</p>
-                <button class="botao-explore">Explore agora</button>
+                <h1 class="texto-gradiente">Cuide do Bem-Estar do seu <br> Amigo de 4 patas !</h1>
+                <h1 class="texto-gradiente "></h1>
+                <p class="paragrafo-gradiente ">Encontre a melhor seleção de rações e medicamentos para <br> manter seu animal
+                    saudável e feliz.</p>
+            </div>
+            <div class="col">
+                <div class="capsula float-end">
+                    <img src="/images/dogs.png" class="img-dog  " alt="">
+                </div>
             </div>
         </div>
 
-        <img src="/images/bem-estar.png" class="dog-image"  alt="">
 
-</div>
+
+
+    </div><!--Fim container-->
+
+
+    <div class="carrocel" style="overflow: hidden"> <!--Início carrocel-->
+
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="div-arredondada">
+                        <h2 style="overflow: hidden">Seu pet é bem cuidado aqui</h2>
+                      </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="div-arredondada">
+                        Seu cu
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="div-arredondada">
+                        minha pica
+                      </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" style="overflow: hidden" href="#carouselExampleControls" role="button"
+                data-slide="prev">
+                <span class="sr-only color-prox"><img class="seta-esquerda" src="/images/seta-carrocel-esquerda.png" width="180" alt=""></span>
+            </a>
+            <a class="carousel-control-next" style="overflow: hidden" href="#carouselExampleControls" role="button"
+                data-slide="next">
+                <span class="sr-only color-prox"><img class="seta-direita" src="/images/seta-carrocel-direita.png" width="180" alt=""></span>
+            </a>
+        </div>
+    </div><!-- Fim Carrocel-->
+
+    <div class="container mt-12 ">
+        <div class="position relative">
+            <div class="shadow p-4 mx-auto" style="max-width:100% ">
+                <div class="position-relative me-2  story">
+                    <img src="" alt="">
+                </div>
+                <div class="storys-texto ">Equinos </div>
+        </div>
+    </div>
 
 
 
--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -9,6 +9,8 @@
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -78,6 +80,15 @@
         </div>
     </div>
     <script src="/js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 
     @yield('content')
 
